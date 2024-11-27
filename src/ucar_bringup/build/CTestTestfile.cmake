6f056a5be19ac488349bc0f1f2b87e69b6175272
QianYuan1437 <zyh42018108@163.com>
2024-11-27 08:11:22 +0800

    Change ucar_bringup ucar_navigation

--- src/ucar_bringup/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iflytek/task_ws/src/ucar_bringup
-# Build directory: /home/iflytek/task_ws/src/ucar_bringup/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
